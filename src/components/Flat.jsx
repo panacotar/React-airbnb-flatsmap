cba23f9c982596a088e82bb34313f69e111556d6
DariusPirvulescu <organicdarius@gmail.com>
2020-08-05 22:13:36 +0300

    added Flat.scss, added props to flat component

--- src/components/Flat.jsx
+++ src/components/Flat.jsx
@@ -1,1 +1,23 @@
 import React from "react";
+import "./Flat.scss";
+
+const Flat = ({ imageUrl, name, price, rating }) => {
+  return (
+    <div className="flat">
+      <div className="flat-img">
+        <img src={imageUrl} alt="Random flat img" />
+      </div>
+      <div className="flat-info">
+        <div className="rating">
+          <p>{rating} ⭐ </p>
+        </div>
+        <div className="description">
+          <p>{name}</p>
+          <p>{price}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Flat;
