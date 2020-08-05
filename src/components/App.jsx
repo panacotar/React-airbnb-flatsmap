fc39bb36cb166f930ee0604b86f312c6efa48a70
DariusPirvulescu <organicdarius@gmail.com>
2020-08-05 20:01:16 +0300

    created Flat, Map, Search components, placed them in the general layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,24 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
+import Search from "./Search";
+import Map from "./Map"
 
-function App() {
+const App = () => {
   return (
-    <h1>HEllo</h1>  
+    <div className="app">
+      <div className="main">
+        <div className="search">
+          <Search className="search-bar" />
+        </div>
+        <div className="flats">
+          <h1>List of Flats here</h1>
+        </div>
+      </div>
+      <div className="map">
+        <Map />
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Search.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Search = () => {
+  // return <input type="text" name="search" placeholder="Search..." />;
+  return (
+    <div className="active-pink-3 active-pink-4 mb-4">
+      <input
+        className="form-control"
+        type="text"
+        placeholder="Search"
+        aria-label="Search"
+      />
+    </div>
+  );
+};
+
+export default Search;
