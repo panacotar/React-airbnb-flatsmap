54d85369180deb5296d9ec11592d8553fb2af6e5
DariusPirvulescu <organicdarius@gmail.com>
2020-08-06 16:32:35 +0300

    imported flats JSON into App and displayed one flat

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./App.scss";
+import flats from "../data/flatsDB.json";
 import Search from "./Search";
-import Map from "./Map"
+import Flat from "./Flat";
+import Map from "./Map";
 
 const App = () => {
+  const { name, lat, lng, price, priceCurrency, imageUrl, rating } = flats[0];
+  console.log(name);
+
   return (
     <div className="app">
       <div className="main">
@@ -11,7 +16,38 @@ const App = () => {
           <Search className="search-bar" />
         </div>
         <div className="flats">
-          <h1>List of Flats here</h1>
+          <Flat
+            imageUrl={imageUrl}
+            name={name}
+            price={price}
+            priceCurrency={priceCurrency}
+            rating={rating}
+          />
+          <Flat
+            imageUrl=""
+            name="My wonderful appartment on edgar street"
+            price={231}
+            priceCurrency={priceCurrency}
+            rating={3}
+          />
+          <Flat
+            imageUrl=""
+            name="My wonderful appartment on edgar street"
+            price={231}
+            rating={3}
+          />
+          <Flat
+            imageUrl=""
+            name="My wonderful appartment on edgar street"
+            price={231}
+            rating={3}
+          />
+          <Flat
+            imageUrl=""
+            name="My wonderful appartment on edgar street"
+            price={231}
+            rating={3}
+          />
         </div>
       </div>
       <div className="map">
